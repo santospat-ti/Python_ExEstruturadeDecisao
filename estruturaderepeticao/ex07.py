88cf9f508d9382ae23f5ab2ef329346d7c1e06ed
Patrícia <58836495+santospat-ti@users.noreply.github.com>
2021-01-27 15:58:34 -0300

    Delete estruturaderepeticao directory

--- estruturaderepeticao/ex07.py
+++
@@ -1,6 +0,0 @@
-#Faça um programa que leia 5 números e informe o maior número.
-
-lista = []
-for x in range(5):
-    lista.append(int(input('Informe um número: ')))
-print(max(lista))
--- estruturaderepeticao/ex08.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que leia 5 números e informe a soma e a média dos números.
-
-lista = []
-for x in range(5):
-    lista.append(int(input('Digite um número: ')))
-    soma = sum(lista)
-    media = soma / len(lista)
-print(media)
